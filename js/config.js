f2587176bc97bf37d195244322337d54e685cbf4
Mr. Coxall <mr.coxall@mths.ca>
2020-10-18 12:18:15 +0000

    added Cognito sign on

---
+++ js/config.js
@@ -0,0 +1,7 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_grJcsuK3I', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-1', // e.g. us-east-2
+		clientId: '1pkvie6aab40lsdc77pdokqgt7' //is this used anywhere?
+    },
+};
